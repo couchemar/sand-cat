6e73153940a91da1b3d0e1c147a225b257e1c37c
Andrey Pavlov <couchemar@yandex.ru>
2014-05-08 13:54:43 +0400

    [master] Base arithmetic operations

--- lib/sand_cat/words.ex
+++ lib/sand_cat/words.ex
@@ -1,8 +1,9 @@
 defmodule SandCat.Words do
   use SandCat.Core
 
-  defword :+, [a, b] do
-   [a + b]
-  end
+  defword :+, [a, b], do: [a + b]
+  defword :-, [a, b], do: [b - a]
+  defword :*, [a, b], do: [b * a]
+  defword :/, [a, b], do: [b / a]
 
 end
